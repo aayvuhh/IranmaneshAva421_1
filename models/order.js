ad93f7a611478c0d15170b6fdfbb62bb8bac67e0
Ava Iranmanesh <ami5520@psu.edu>
2026-04-19 21:46:02 -0400

    Create order model with schema for MongoDB

---
+++ models/order.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+  items: [{ type: String, required: true }],
+  totalAmount: { type: Number, required: true },
+  status: { type: String, enum: ['Pending', 'Paid', 'Cancelled'], default: 'Pending' },
+  paymentMethod: { type: String }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
